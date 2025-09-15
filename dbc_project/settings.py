d5e1e9448946cad10e3a676317d9300f21a43840
vs <cabrera.martin9393@gmail.com>
2025-09-15 05:24:18 -0300

    Debug: imprimo ALLOWED_HOSTS en logs

--- dbc_project/settings.py
+++ dbc_project/settings.py
@@ -135,3 +135,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+print("ALLOWED_HOSTS:", ALLOWED_HOSTS)
