816f777d00af8c88dc207d9b884a0e91be6770a5
vs <cabrera.martin9393@gmail.com>
2025-09-15 12:16:55 -0300

    Agregado www.drivenbycode.cl a ALLOWED_HOSTS para evitar rechazo por encabezado

--- dbc_project/settings.py
+++ dbc_project/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,drivenbycode.cl,drivenbycode.onrender.com').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 
