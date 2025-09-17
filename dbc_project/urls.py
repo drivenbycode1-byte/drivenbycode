398c002de1a623d00c17e82d78f3d5055e0529e4
vs <cabrera.martin9393@gmail.com>
2025-09-17 08:37:48 -0300

    Corrijo import roto: conecto login_oculto_panel en urls

--- dbc_project/urls.py
+++ dbc_project/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from dbc_app.views import acceso_panel, login_oculto
+from dbc_app.views import acceso_panel, login_oculto_panel
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('perro_verde_sucio/', admin.site.urls),
     path('el-perro-verde/<str:token>/', acceso_panel),
-    path('acceso-silencioso/<str:token>/', login_oculto),
+    path('acceso-silencioso/<str:token>/', login_oculto_panel),
     path('', include('dbc_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
