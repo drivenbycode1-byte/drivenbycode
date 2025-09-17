85931fbd984be106a93d85202dd10d4a7ccce336
vs <cabrera.martin9393@gmail.com>
2025-09-17 09:24:12 -0300

    Blindaje completo: ajustes técnicos, visuales y simbólicos

--- dbc_project/urls.py
+++ dbc_project/urls.py
@@ -6,7 +6,7 @@ from dbc_app.views import acceso_panel, login_oculto
 from two_factor.urls import urlpatterns as tf_urls
 
 urlpatterns = [
-    #path('', include(tf_urls)),  # ← activa el flujo extendido con QR
+    path('', include(tf_urls)),  # ← activa el flujo extendido
     path('perro_verde_sucio/', admin.site.urls),
     path('el-perro-verde/<str:token>/', acceso_panel),
     path('acceso-silencioso/<str:token>/', login_oculto),
