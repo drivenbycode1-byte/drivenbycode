9ddae5e7d3fdc3af7ecf4d8a570bf29ccba99dfb
vs <cabrera.martin9393@gmail.com>
2025-09-17 08:57:37 -0300

    Integración de rutas 2FA: activación del guardián y flujo extendido

--- dbc_project/urls.py
+++ dbc_project/urls.py
@@ -3,12 +3,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from dbc_app.views import acceso_panel, login_oculto
+from two_factor.urls import urlpatterns as tf_urls
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('', include(tf_urls)),  # ← activa el flujo extendido con QR
     path('perro_verde_sucio/', admin.site.urls),
     path('el-perro-verde/<str:token>/', acceso_panel),
     path('acceso-silencioso/<str:token>/', login_oculto),
     path('', include('dbc_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
