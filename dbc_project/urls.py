4a0b0858d199a05fe90dbdb90e332bfbfa28a1d7
vs <cabrera.martin9393@gmail.com>
2025-09-17 05:27:42 -0300

    Limpieza de rutas y vistas no utilizadas para consolidar el blindaje

--- dbc_project/urls.py
+++ dbc_project/urls.py
@@ -1,18 +1,12 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from two_factor.urls import urlpatterns as tf_urls
-from django.shortcuts import redirect
-from django.contrib.auth.decorators import login_required
 from dbc_app.views import acceso_panel, login_oculto
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('el-perro-verde/', acceso_panel),
-    path('', include(tf_urls)),
-    path('', include('dbc_app.urls')),
+    path('el-perro-verde/<str:token>/', acceso_panel),
     path('acceso-silencioso/<str:token>/', login_oculto),
+    path('', include('dbc_app.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
