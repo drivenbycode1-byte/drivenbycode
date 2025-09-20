292265f5f7322f4078a27aeebf86d2204c2fdf69
drivenbycode1-byte <drivenbycode.1@gmail.com>
2025-09-20 11:24:57 -0300

    Update urls.py

--- dbc_app/urls.py
+++ dbc_app/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path
 from . import views
+from .views import dashboard
 
 
 app_name = 'dbc_app'
@@ -9,6 +10,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('indice/', views.indice, name='indice'),
     path('indice/<int:dbc_id>', views.proyectos, name='proyectos'),
-    path('todos/', views.todos_los_posts, name='todos_los_posts'),    
+    path('todos/', views.todos_los_posts, name='todos_los_posts'),
+    path('ding_dong_dashboard/', dashboard, name='dashboard'),
     #path('healthz/', views.health_check),
 ]
